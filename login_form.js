17b7646009bec21cdd2509c9d19199bcc0cfa39f
Sprakis <71209128+Sprakis@users.noreply.github.com>
2023-10-05 08:58:47 +0300

    Update login_form.js

--- login_form.js
+++ login_form.js
@@ -2,13 +2,13 @@ function start_func() {
 	let telegramApp = window.Telegram.WebApp;
 	document.querySelector('body').style.backgroundColor = telegramApp.themeParams.secondary_bg_color;
 	telegramApp.expand();
-	telegramApp.MainButton.text = "TestMainButton";
+	telegramApp.MainButton.text = "Sign in";
 	telegramApp.MainButton.color = "#ff0000";
 	telegramApp.MainButton.show();
 	telegramApp.MainButton.onClick(function send()
 		let login = document.getElementById('username').value;
 		let pass = document.getElementById('password').value;
-		let kek = '{\n\t"login": "' + login + '",\n\t"pass": "' + pass + '"\n}';
-		telegramApp.sendData(kek);
+		let creds = '{\n\t"login": "' + login + '",\n\t"pass": "' + pass + '"\n}';
+		telegramApp.sendData(creds);
 	})
 };
